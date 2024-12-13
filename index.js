5a7f9aef3f7a5b68cdd3e1ed14ced4ba472739df
impressivecoder01 <rohanrahman251@gmail.com>
2024-12-14 01:17:31 +0600

    jobApplication api added

--- index.js
+++ index.js
@@ -54,6 +54,16 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/job_application', async(req, res) => {
+      const email = req.query.email;
+      const query = {applicant_email: email}
+      const result = await jobApplicationCollection.find(query).toArray()
+
+      // not the best way
+      
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
